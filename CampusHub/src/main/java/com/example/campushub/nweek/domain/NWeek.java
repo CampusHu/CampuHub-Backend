829104f333e2cb096030189813661b2f7cf04bb3
yoonwonsik160317 <wssesang@gmail.com>
2025-01-06 21:29:03 +0900

    n주차 빌더 생성

--- CampusHub/src/main/java/com/example/campushub/nweek/domain/NWeek.java
+++ CampusHub/src/main/java/com/example/campushub/nweek/domain/NWeek.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -31,4 +32,10 @@ public class NWeek {
 
 	@Enumerated(EnumType.STRING)
 	private Week week;
+
+	@Builder
+	public NWeek(Course course, Week week) {
+		this.course = course;
+		this.week = week;
+	}
 }
