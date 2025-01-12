7735beb501c16f5bb872e9cb0aebc84a099b8ff1
전영욱 <jeon-young-wook@jeon-yeong-ug-ui-MacBookAir.local>
2025-01-12 20:44:44 +0900

    장학 등록 수정 필요

--- CampusHub/src/main/java/com/example/campushub/scholarship/repository/ScholarshipRepositoryCustom.java
+++ CampusHub/src/main/java/com/example/campushub/scholarship/repository/ScholarshipRepositoryCustom.java
@@ -2,10 +2,13 @@ package com.example.campushub.scholarship.repository;
 
 import com.example.campushub.scholarship.dto.ScholarshipResponseDto;
 import com.example.campushub.scholarship.dto.ScholarshipSearchCondition;
+import com.example.campushub.user.domain.User;
 
 import java.util.List;
 
 public interface ScholarshipRepositoryCustom {
 
     List<ScholarshipResponseDto> findAllByCondition(ScholarshipSearchCondition cond);
+
+
 }
