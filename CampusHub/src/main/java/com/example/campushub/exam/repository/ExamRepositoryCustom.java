d1ec09fc722756ca54ecbec80ce29043b1826e72
yoonwonsik160317 <wssesang@gmail.com>
2025-01-31 14:17:39 +0900

    성적 수정

--- CampusHub/src/main/java/com/example/campushub/exam/repository/ExamRepositoryCustom.java
+++ CampusHub/src/main/java/com/example/campushub/exam/repository/ExamRepositoryCustom.java
@@ -2,12 +2,13 @@ package com.example.campushub.exam.repository;
 
 import com.example.campushub.exam.dto.ExamFindAllResponse;
 import com.example.campushub.exam.dto.ExamScoreInputRequest;
+import com.example.campushub.exam.dto.ExamSearchCondition;
 
 import java.util.List;
 
 public interface ExamRepositoryCustom {
 
-    List<ExamFindAllResponse> findExamScoresByUserCourseId(Long userCourseId);
+    List<ExamFindAllResponse> findExamScoresByUserNums(List<String> userNums, ExamSearchCondition cond);
 
     void updateExamScore(ExamScoreInputRequest request);
 }
