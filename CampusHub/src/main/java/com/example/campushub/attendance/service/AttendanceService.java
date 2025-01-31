e65ad94cfcd802deed1a29e0506c9ac8511d0f85
전영욱 <hday19941@naver.com>
2025-02-01 04:29:44 +0900

    출석 서비스 수정

--- CampusHub/src/main/java/com/example/campushub/attendance/service/AttendanceService.java
+++ CampusHub/src/main/java/com/example/campushub/attendance/service/AttendanceService.java
@@ -52,7 +52,7 @@ public class AttendanceService {
     }
 
 
-    //드롭다운을 통한 출결 설정
+    //드롭다운을 통한 출결 설정.
     @Transactional
     public void createAttendance(LoginUser loginUser, List<AttendanceRequestDto> requestDto,AttendanceSearchCondition cond) {
         //교수인지 확인
